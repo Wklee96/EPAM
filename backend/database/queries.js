000b79ab213c148d2876253285248d18f7db4130
wklee96 <lweikang96@gmail.com>
2019-12-08 14:01:55 +0800

    add hash

--- backend/database/queries.js
+++ backend/database/queries.js
@@ -1,7 +1,7 @@
 const database = {};
 
 database.queries = {
-  get_bond_type: "SELECT * FROM bonds WHERE bond_type=$1;",
+  get_bond_type: "SELECT issuer, grade, maturity_date, coupon, agency, bond_type, digest(issuer::TEXT, 'sha256')::TEXT as hash, digest(coupon::TEXT, 'sha256')::TEXT as prehash FROM bonds WHERE bond_type=$1;",
 
   insert_bond: "INSERT INTO bonds VALUES ($1, $2, $3, $4, $5, $6);"
 };
